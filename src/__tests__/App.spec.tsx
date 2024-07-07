115f71bda2f4fc5eca768b959554817b181b9fab
dhythm <yuta.okada.20@gmail.com>
2024-07-07 17:51:47 +0900

    implement test with RTL [WIP]

--- src/__tests__/App.spec.tsx
+++ src/__tests__/App.spec.tsx
@@ -1,1 +1,9 @@
+import { render, screen } from "@testing-library/react";
+import "@testing-library/jest-dom";
+import App from "../App";
 
+test("", async () => {
+  render(<App />);
+
+  expect(screen.getByRole("button")).toBeEnabled();
+});
